c544c90e0c3a2b02aaf70f1bea28b8c78dc6ac8c
LalithKishore-S <lalithkishore.sridhar@gmail.com>
2026-03-15 12:21:22 +0530

    MDP formualtion added

---
+++ MDP.py
@@ -0,0 +1,27 @@
+
+class MDP:
+    def __init__(self, topics, difficulty_types, q_types):
+        self.int_to_actions = {}
+        self.actions_to_int = {}
+        self.n_actions = len(topics) * len(difficulty_types) * len(q_types)
+
+        for topic in topics:
+            for diff_type in difficulty_types:
+                for q_type in q_types:
+                    combo = (topic, diff_type, q_type)
+                    val = len(self.int_to_actions)
+                    self.actions_to_int[combo] = val
+                    self.int_to_actions[val] = combo
+
+    def compute_reward(self, ks, topic, score, prev_score):
+        #ks -> obj of KnowledgeState
+        improvement = score - prev_score
+        if ks.is_neglected(topic):
+            coverage_bonus = 1.0
+        elif ks.trend(topic) < 0:
+            coverage_bonus = 0.5
+        else:
+            coverage_bonus = 0.0
+        
+        mastery_penalty = 1.0 if ks.is_mastered(topic) else 0.0
+        return self.w1 * improvement + self.w2 * coverage_bonus - self.w3 * mastery_penalty
