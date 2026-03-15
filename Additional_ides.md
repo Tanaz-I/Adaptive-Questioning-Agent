378a9b1e4eac025ae37ec6f20cb484e6ad680f2c
LalithKishore-S <lalithkishore.sridhar@gmail.com>
2026-03-15 11:12:55 +0530

    Modified for thresh per topic difficulty

---
+++ Additional_ides.md
@@ -0,0 +1,3 @@
+`knowledge_state.py`
+- mastery level for every topic is done using a threshold of 0.85 which is rigid for all topics. Need to see other measures. ( made topic difficulty wise thresholds)
+- Additional => Should make another RL model that can refine the topic per user.
--- knowledge_state.py
+++ knowledge_state.py
@@ -4,14 +4,16 @@ from collections import defaultdict, deque
 
 class KnowledgeState:
     
-    def __init__(self, topics, window_size):        
-        self.topics  = topics
+    def __init__(self, topics_difficulty, window_size):        
+        self.topics_difficulty = topics_difficulty # dictionary of topic:difficulty
+        self.topics = list(self.topics_difficulty.keys())
         self.window_size  = window_size  #window size for the trend
-        self.num_topics = len(topics)
+        self.num_topics = len(topics_difficulty)
         self.topic_score = defaultdict(float)
         self.recent_scores = defaultdict(lambda: deque(maxlen=window_size))  
         self.attempts = defaultdict(int)
         self.prev_qtype = defaultdict(lambda: (None,None))
+        self.master_thresh = {'basic' : 0.90, 'intermediate' : 0.85, 'advanced':0.75}
 
     def update(self, topic, score, difficulty,question_type):
         
@@ -28,9 +30,11 @@ class KnowledgeState:
         slope = float(np.polyfit(x, recent, 1)[0])
         return slope
 
-    def is_mastered(self, topic, threshold = 0.85,min_attempts= 3):
-        return (self.attempts[topic] >= min_attempts and
-                self.topic_score[topic] >= threshold)
+    def is_mastered(self, topic,min_attempts= 3):
+        topic_difficulty = self.topics_difficulty[topic]
+        threshold = self.master_thresh[topic_difficulty]
+        trend_eval = self.trend(topic)
+        return (self.attempts[topic] >= min_attempts and self.topic_score[topic] >= threshold and self.trend_eval >= 0)
 
     def is_neglected(self, topic):
         return self.attempts[topic] == 0
