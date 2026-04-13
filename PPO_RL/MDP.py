ae618f0882bd6f7d686e0673730cb83ebf3ac538
Livin Joseph <97300264+livin-joseph@users.noreply.github.com>
2026-04-13 15:20:20 +0530

    Modify compute_reward method

--- PPO_RL/MDP.py
+++ PPO_RL/MDP.py
@@ -24,43 +24,52 @@ class MDP:
     def decode(self, action_idx):
         return self.int_to_actions.get(action_idx, -1)
     
-    def compute_reward(self, ks, topic, score, prev_score, old_earned_diff, old_earned_qtype):
+    # def compute_reward(self, ks, topic, score, prev_score, old_earned_diff, old_earned_qtype):
     
-        improvement = score - prev_score
-        if score < 0.3 and ks.attempts[topic] > 3:
-            improvement -= 0.2
+    #     improvement = score - prev_score
+    #     if score < 0.3 and ks.attempts[topic] > 3:
+    #         improvement -= 0.2
 
-        if ks.is_neglected(topic):
-            coverage_bonus = 1.0
-        elif ks.attempts[topic] > 5 and ks.topic_score[topic] < 0.2:
-            coverage_bonus = -0.5
-        elif ks.attempts[topic] < 5:
-            coverage_bonus = 0.7
-        elif ks.trend(topic) < 0:
-            coverage_bonus = 0.4
-        else:
-            coverage_bonus = 0.0
+    #     if ks.is_neglected(topic):
+    #         coverage_bonus = 1.0
+    #     elif ks.attempts[topic] > 5 and ks.topic_score[topic] < 0.2:
+    #         coverage_bonus = -0.5
+    #     elif ks.attempts[topic] < 5:
+    #         coverage_bonus = 0.7
+    #     elif ks.trend(topic) < 0:
+    #         coverage_bonus = 0.4
+    #     else:
+    #         coverage_bonus = 0.0
 
-        prereq_bonus = 0.0
-        for other_topic in ks.topics:
-            prereqs = ks.prerequisites_topic.get(other_topic, [])
-            if topic in prereqs and not ks.prerequisites_met(other_topic):
-                prereq_bonus += 0.2 * ks.topic_score[topic]
+    #     prereq_bonus = 0.0
+    #     for other_topic in ks.topics:
+    #         prereqs = ks.prerequisites_topic.get(other_topic, [])
+    #         if topic in prereqs and not ks.prerequisites_met(other_topic):
+    #             prereq_bonus += 0.2 * ks.topic_score[topic]
 
-        unlock_bonus = 0.0
-        if ks.prerequisites_met(topic) and ks.attempts[topic] == 1:
-            unlock_bonus = 0.8
+    #     unlock_bonus = 0.0
+    #     if ks.prerequisites_met(topic) and ks.attempts[topic] == 1:
+    #         unlock_bonus = 0.8
 
-        new_earned_diff  = ks.current_level[topic]['earned_diff_idx']
-        new_earned_qtype = ks.current_level[topic]['earned_qtype_idx']
-        advancement_bonus = 0.2 if (new_earned_diff > old_earned_diff or 
-                                    new_earned_qtype > old_earned_qtype) else 0.0
+    #     new_earned_diff  = ks.current_level[topic]['earned_diff_idx']
+    #     new_earned_qtype = ks.current_level[topic]['earned_qtype_idx']
+    #     advancement_bonus = 0.2 if (new_earned_diff > old_earned_diff or 
+    #                                 new_earned_qtype > old_earned_qtype) else 0.0
 
-        mastery_penalty = 1.0 if ks.is_mastered(topic) else 0.0
+    #     mastery_penalty = 1.0 if ks.is_mastered(topic) else 0.0
 
-        return (self.w1 * (0.6 * score + 0.4 * improvement)
-            + self.w2 * coverage_bonus
-            + self.w4 * advancement_bonus
-            + self.w5 * prereq_bonus
-            + self.w2 * unlock_bonus
-            - self.w3 * mastery_penalty)
+    #     return (self.w1 * (0.6 * score + 0.4 * improvement)
+    #         + self.w2 * coverage_bonus
+    #         + self.w4 * advancement_bonus
+    #         + self.w5 * prereq_bonus
+    #         + self.w2 * unlock_bonus
+    #         - self.w3 * mastery_penalty)
+
+    def compute_reward(self, ks, topic, score, prev_score, old_earned_diff, old_earned_qtype):
+        # Reward = improvement in overall knowledge state
+        new_mastered   = sum(1 for t in ks.topics if ks.is_mastered(t))
+        total_topics   = len(ks.topics)
+        prev_avg_score = ks.topic_score[topic]
+        avg_score_gain = score - prev_avg_score  # improvement this step
+        
+        return (0.6 * avg_score_gain) + (0.4 * new_mastered / total_topics)
